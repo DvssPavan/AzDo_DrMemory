0bfb1f99871b99802fd2adcf0ab43002819fcb73
DvssPavan <43805820+DvssPavan@users.noreply.github.com>
2022-07-15 16:58:17 +0530

    Update DrMemory.py

--- DrMemory.py
+++ DrMemory.py
@@ -148,7 +148,7 @@ class DrMemoryTask:
     """Get and parse the logs"""
     def get_logs(self, filePath):
         # read path from Shared folder
-        remotezip = self.shared_folder_path + filePath[filePath.find("archive\\")] + "\\log.zip"
+        remotezip = self.shared_folder_path + filePath[filePath.find("archive\\"):] + "\\log.zip"
 
         # open remote log.zip file
         os.chmod(remotezip, 0o777)
